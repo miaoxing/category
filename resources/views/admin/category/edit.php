ba530b5ad97f3ba305c671b57b14a00fe33d08e6
twinh <twinhuang@qq.com>
2016-12-14 20:47:52 +0800

    为.btn增加默认类名  #2203

--- resources/views/admin/category/edit.php
+++ resources/views/admin/category/edit.php
@@ -121,7 +121,7 @@
             提交
           </button>
           &nbsp; &nbsp; &nbsp;
-          <a class="btn" href="<?= $url('admin/category/index') ?>">
+          <a class="btn btn-default" href="<?= $url('admin/category/index') ?>">
             <i class="fa fa-undo bigger-110"></i>
             返回列表
           </a>
