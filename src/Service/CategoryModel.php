7e6e60732cfbb8e7f07bf0834dfeaae29ea79b59
twinh <twinhuang@qq.com>
2018-08-25 16:02:42 +0800

    feat: 覆盖setPlainPassword方法

--- src/Service/CategoryModel.php
+++ src/Service/CategoryModel.php
@@ -3,14 +3,24 @@
 namespace Miaoxing\Category\Service;
 
 use Miaoxing\Category\Metadata\CategoryTrait;
-use Miaoxing\Plugin\BaseModelV2;
+use Miaoxing\Plugin\Model\CastTrait;
+use Miaoxing\Plugin\Model\GetSetTrait;
+use Miaoxing\Plugin\Model\QuickQueryTrait;
 
 /**
  * CategoryModel
  */
-class CategoryModel extends BaseModelV2
+class CategoryModel extends Category
 {
     use CategoryTrait;
+    use CastTrait;
+    use QuickQueryTrait;
+    use GetSetTrait;
 
     protected $table = 'category';
+
+    public function afterFind()
+    {
+        //
+    }
 }
