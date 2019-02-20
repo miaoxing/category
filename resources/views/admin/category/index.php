40e91b1c6ef4928ebd39dc3f76f40c0235efde36
twinh <twinhuang@qq.com>
2019-02-20 17:31:53 +0800

    refactoring: deparam => unparam

--- resources/views/admin/category/index.php
+++ resources/views/admin/category/index.php
@@ -79,7 +79,7 @@
     personal: '个人中心',
     mall: '商城'
   };
-  require(['plugins/admin/js/data-table', 'form', 'jquery-deparam'], function () {
+  require(['plugins/admin/js/data-table', 'form', 'jquery-unparam'], function () {
     var recordTable = $('#category-table').dataTable({
       ajax: {
         url: $.queryUrl('admin/category.json')
