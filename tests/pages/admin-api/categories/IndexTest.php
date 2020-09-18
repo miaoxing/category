3f46d7867387fea09169aa2b759e5a87701b6ba1
twinh <twinhuang@qq.com>
2020-09-18 11:26:59 +0800

    test(category): 测试创建接口

--- tests/pages/admin-api/categories/IndexTest.php
+++ tests/pages/admin-api/categories/IndexTest.php
@@ -35,4 +35,40 @@ class IndexTest extends BaseTestCase
 
         $this->assertSame($category->toArray(), $ret['data'][0], '返回列表数据包含下级');
     }
+
+    public function testPost()
+    {
+        User::loginById(1);
+
+        $ret = Tester::postAdminApi('categories', [
+            'id' => '1',
+            'name' => '测试',
+            'sort' => '60',
+            'description' => '描述',
+        ]);
+        $this->assertRetSuc($ret);
+
+        /** @var CategoryModel $category */
+        $category = $ret['data'];
+        $this->assertNotEquals('1', $category->id);
+        $this->assertEquals('测试', $category->name);
+        $this->assertEquals(60, $category->sort);
+        $this->assertEquals('描述', $category->description);
+    }
+
+    public function testPostCreateSubCategory()
+    {
+        User::loginById(1);
+
+        $category = CategoryModel::save();
+
+        $ret = Tester::postAdminApi('categories', [
+            'parentId' => $category->id,
+        ]);
+        $this->assertRetSuc($ret);
+
+        /** @var CategoryModel $subCategory */
+        $subCategory = $ret['data'];
+        $this->assertSame($category->id, $subCategory->parentId);
+    }
 }
