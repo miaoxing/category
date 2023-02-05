9a37cfcfc78d55f8b477515d1e9a0ecd96076ff0
twinh <twinhuang@qq.com>
2023-02-05 20:42:25 +0800

    refactor: 更新后台菜单

--- src/CategoryPlugin.php
+++ src/CategoryPlugin.php
@@ -2,6 +2,8 @@
 
 namespace Miaoxing\Category;
 
+use Miaoxing\Admin\Service\AdminMenu;
+
 class CategoryPlugin extends \Miaoxing\Plugin\BasePlugin
 {
     protected $name = '分类';
@@ -9,4 +11,21 @@ class CategoryPlugin extends \Miaoxing\Plugin\BasePlugin
     protected $description = '用于商品分类';
 
     protected $code = 206;
+
+    public function onAdminMenuGetMenus(AdminMenu $menu)
+    {
+        $product = $menu->child('product');
+
+        $categories = $product->addChild()->setLabel('分类管理')->setUrl('admin/categories')->setApis([
+            'GET admin-api/categories',
+        ]);
+        $categories->addChild()->setUrl('admin/categories/new')->setLabel('添加')->setApis([
+            'GET admin-api/categories/defaults',
+            'POST admin-api/categories',
+        ]);
+        $categories->addChild()->setUrl('admin/categories/[id]/edit')->setLabel('编辑')->setApis([
+            'GET admin-api/categories/[id]',
+            'PATCH admin-api/categories/[id]',
+        ]);
+    }
 }
