fce981a8c23a22482d8260d7513b1b150de4b5b5
twinh <twinhuang@qq.com>
2024-07-14 15:47:05 +0800

    feat(u): 调整页面内容为卡片视图

--- pages/admin/categories/new.jsx
+++ pages/admin/categories/new.jsx
@@ -1,11 +1,12 @@
 /**
  * @share [id]/edit
  */
-import {CListBtn} from '@mxjs/a-clink';
-import {Page, PageActions} from '@mxjs/a-page';
-import {Form, FormItem, FormAction} from '@mxjs/a-form';
+import { CListBtn } from '@mxjs/a-clink';
+import { Page, PageActions } from '@mxjs/a-page';
+import { Form, FormAction, FormItem } from '@mxjs/a-form';
 import Input from '@mxjs/a-input';
-import {Select, FormItemSort} from '@miaoxing/admin';
+import { FormItemSort, Select } from '@miaoxing/admin';
+import { Section } from '@mxjs/a-section';
 
 const New = () => {
   return (
@@ -15,17 +16,19 @@ const New = () => {
       </PageActions>
 
       <Form>
-        <FormItem label="父级分类" name="parentId">
-          <Select url="categories" labelKey="name" valueKey="id" firstLabel="根分类" firstValue=""/>
-        </FormItem>
-        <FormItem label="名称" name="name" required>
-          <Input maxLength={255}/>
-        </FormItem>
-        <FormItem label="简介" name="description">
-          <Input.TextArea maxLength={255}/>
-        </FormItem>
-        <FormItemSort/>
-        <FormItem name="id" type="hidden"/>
+        <Section>
+          <FormItem label="父级分类" name="parentId">
+            <Select url="categories" labelKey="name" valueKey="id" firstLabel="根分类" firstValue=""/>
+          </FormItem>
+          <FormItem label="名称" name="name" required>
+            <Input maxLength={255}/>
+          </FormItem>
+          <FormItem label="简介" name="description">
+            <Input.TextArea maxLength={255}/>
+          </FormItem>
+          <FormItemSort/>
+        </Section>
+
         <FormAction/>
       </Form>
     </Page>
